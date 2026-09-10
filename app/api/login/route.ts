3d174bcb916aa75a3dde00b2fad5b5b917c4d245
KamilaHryc <139580424+KamilaHryc@users.noreply.github.com>
2026-09-10 17:27:44 +0200

    Podlaczenie backendu Railway

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
     const body = await request.text();
 
     const response = await fetch(
-      "https://hurtownia-warzywa.42web.io/hurtownia-api/login.php",
+     "https://hurtownia-api-production.up.railway.app/login.php",
       {
         method: "POST",
         headers: {
